9831a4045c0d33200aaffe25c69e4b27e80affde
Arghun <arghava.mousanezhad@utopiamusic.com>
2023-10-16 18:26:27 +0200

    fix(home): delete . from heading and description of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <div className="bg-black w-screen h-screen flex justify-center items-center text-white">
       <div className="w-full mx-auto max-w-[600px] flex flex-col items-center">
-        <h1 className="text-6xl">The best journal app.</h1>
+        <h1 className="text-6xl">The best journal app</h1>
         <p className="text-2xl text-white/60 my-6">
-          This is the best app for tracking your mood.
+          This is the best app for tracking your mood
         </p>
         <Link href="/journal">
           <button className="bg-blue-600 p-2 rounded-lg mt-8">
